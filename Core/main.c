3bca277f356eaac8deef7fd60156d0c6dd232632
kochetkov0 <v.kochetkov@luxystesh.com>
2020-09-19 14:22:11 -0400

    Initial parsing has been added

--- Core/main.c
+++ Core/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "mpc.h"
 /* If we are compiling on Windows compile these functions */
 #ifdef _WIN32
 #include <string.h>
@@ -26,21 +26,84 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+
 int main(int argc, char** argv) {
 
-  puts("Urban Version 0.0.0.0.1");
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+  Number, Operator, Expr, Lispy);
+
+  
+  puts("Urban Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
     
-    /* Now in either case readline will be correctly defined */
+
     char* input = readline("urban>");
     add_history(input);
 
-    printf("No you're a %s\n", input);
-    free(input);
+    mpc_result_t r;
+    if(mpc_parse("<stdin>",input,Lispy,&r)){
+
+
+ 
+      
+      long result = eval(r.output);
+      printf("%li\n",result);
+      mpc_ast_delete(r.output);
+    }else{
+
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+
+    }
+
 
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   
   return 0;
 }
